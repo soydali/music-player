9ff75b73853d66b1066dbb990cd2a7e9c86b2a42
Soy Dali <efekocxx@gmail.com>
2024-11-04 18:09:27 +0300

    First

---
+++ script.js
@@ -0,0 +1,93 @@
+var play = false
+bar = $(".progress")
+music = $("audio")[0]
+button = $("#play")
+
+$("#play").click(function () {
+    
+
+
+    if (play) {
+        button.removeClass("fa-pause")
+        button.addClass("fa-play")
+        play = false
+        music.pause()
+    } else {
+        button.removeClass("fa-play")
+        button.addClass("fa-pause")
+        play = true
+        music.play()
+        function fresh() {
+            barLength = (music.currentTime * 100) / music.duration
+            bar.css({ "width": `${barLength}%` })
+            requestAnimationFrame(fresh)
+            if (barLength == 100) {
+                button.removeClass("fa-pause")
+                button.addClass("fa-play")
+                music.pause()
+                music.currentTime = 0
+                play = false
+            }
+        }
+        requestAnimationFrame(fresh)
+    }
+});
+
+$(".player-progress").click(function (e) {
+    progressBar = $(this)
+    let x = e.offsetX;
+    width = progressBar.innerWidth()
+    music.currentTime = music.duration / width * x
+    bar.css({ "width": `${(music.currentTime * 100) / music.duration}%` })
+
+})
+
+index = 0
+
+$(".fa-forward").click(function () {
+    $.get("/data.json", function (data) {
+        if (index == (data.length - 1)){
+            index = 0
+        }else{
+            index = index + 1
+        }
+        play = false
+        button.removeClass("fa-pause")
+        button.addClass("fa-play")
+        $(".player-info h1").text(data[index].name)
+        $(".player-image").css({"background":`url(${data[index].img})`, "background-size": "cover", "background-position": "center"})
+        $(".bg-image").css({"background":`url(${data[index].img})`, "background-size": "cover", "background-position": "center"})
+        music.src = data[index].src
+    });
+})
+
+
+// eski
+// var play = false
+// var interval
+
+// $("#play").click(function () {
+//     button = $(this)
+
+//     music = $("audio")[0]
+
+//     if (play) {
+//         button.removeClass("fa-pause")
+//         button.addClass("fa-play")
+//         play = false
+//         music.pause()
+
+//         clearInterval(interval);
+//     } else {
+//         button.removeClass("fa-play")
+//         button.addClass("fa-pause")
+//         play = true
+//         music.play()
+//         interval = setInterval(function () {
+//             bar = $(".progress")
+//             barLength = (music.currentTime * 100) / music.duration
+//             bar.css({"width": `${barLength}%`})
+//         }, 100);
+//     }
+
+// });
